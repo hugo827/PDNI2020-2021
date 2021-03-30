1503b89605c42ac32cd62dba2f23ab6d155c838e
Robin471 <81297957+Robin471@users.noreply.github.com>
2021-03-30 16:50:49 +0200

    Update Display.c

--- Projet/Projet/Display.c
+++ Projet/Projet/Display.c
@@ -3,7 +3,7 @@
 
 void displayResultsForEachClasses(int theoricalAppearencePerClass[], int correctResultsPerClass[], double percentageCorrectResultPerClass[]) {
 	printf("         classe        |       bien classes    |          total        |       Pourcentage    \n");
-	// 20 charactères par colonne
+	// 20 charactÃ¨res par colonne
 	
 	for (int i = 0; i < NB_CLASSES; i++) {
 		if (theoricalAppearencePerClass[i] != 0) {
@@ -27,3 +27,20 @@ void displayClass(int theoricalAppearencePerClass[], int resultsPerClass[NB_CLAS
 	printf("Montee       %4d    |     %4d       %4d        %4d        %4d        %4d\n", theoricalAppearencePerClass[MONTEE], resultsPerClass[JOGGING][MONTEE], resultsPerClass[MARCHE][MONTEE], resultsPerClass[ASSIS][MONTEE], resultsPerClass[MONTEE][MONTEE], resultsPerClass[DESCENTE][MONTEE]);
 	printf("Descendre    %4d    |     %4d       %4d        %4d        %4d        %4d\n", theoricalAppearencePerClass[DESCENTE], resultsPerClass[JOGGING][DESCENTE], resultsPerClass[MARCHE][DESCENTE], resultsPerClass[ASSIS][DESCENTE], resultsPerClass[MONTEE][DESCENTE], resultsPerClass[DESCENTE][DESCENTE]);
 }
+
+/* Alternative 
+void displayClass(int theoricalAppearencePerClass[], int resultsPerClass[NB_CLASSES][NB_CLASSES]) {
+	printf("\nA classer            |  ");
+	for (int i = 0; i < NB_CLASSES; i++) {
+		printf("        %d  ", i+1);
+	}
+	printf("\n====================================================================================\n");
+	printf("Numclasse    Nombre  |\n");
+	for (int i = 0; i < NB_CLASSES; i++) {
+		printf("    %d       %4d     |", i + 1, theoricalAppearencePerClass[i]);
+		for (int j = 0; j < NB_CLASSES; j++) {
+			printf("       %4d", resultsPerClass[j][i]);
+		}
+		printf("\n");
+	}
+} */
